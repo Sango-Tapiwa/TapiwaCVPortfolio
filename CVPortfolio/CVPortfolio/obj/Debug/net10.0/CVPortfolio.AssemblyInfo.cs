86594d7112688d7ca47466871e371531328da622
Sango-Tapiwa <128785990+Sango-Tapiwa@users.noreply.github.com>
2026-02-23 17:25:56 +0200

    Docker file commit
    Added docker files

--- CVPortfolio/CVPortfolio/obj/Debug/net10.0/CVPortfolio.AssemblyInfo.cs
+++ CVPortfolio/CVPortfolio/obj/Debug/net10.0/CVPortfolio.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("b36734d6-3fea-4b5c-b7f7-b81ec4cebd1d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
